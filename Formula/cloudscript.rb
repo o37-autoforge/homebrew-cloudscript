daedd77be694dd685ee9f49044f69ebe8e36449b
GitHub Action <action@github.com>
2024-12-20 07:00:38 +0000

    Update formula to version v0.0.9

--- Formula/cloudscript.rb
+++ Formula/cloudscript.rb
@@ -1,20 +1,20 @@
 class Cloudscript < Formula
     desc "Cloudscript CLI installable with brew"
     homepage "https://github.com/Panchangam18/test-cloudscript-brew"
-    version "0.0.8"
+    version "0.0.9"
   
     on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/Panchangam18/test-cloudscript-brew/releases/download/v0.0.8/cloud-cli-darwin-arm64.tar.gz"
+        url "https://github.com/Panchangam18/test-cloudscript-brew/releases/download/v0.0.9/cloud-cli-darwin-arm64.tar.gz"
         sha256 "e8510d3654ba57c4c4dd011cbd19661bcaf3bd4f224d51838341a0e8d314948b"
       else
-        url "https://github.com/Panchangam18/test-cloudscript-brew/releases/download/v0.0.8/cloud-cli-darwin-amd64.tar.gz"
+        url "https://github.com/Panchangam18/test-cloudscript-brew/releases/download/v0.0.9/cloud-cli-darwin-amd64.tar.gz"
         sha256 "e8510d3654ba57c4c4dd011cbd19661bcaf3bd4f224d51838341a0e8d314948b"
       end
     end
   
     on_linux do
-      url "https://github.com/Panchangam18/test-cloudscript-brew/releases/download/v0.0.8/cloud-cli-linux-amd64.tar.gz"
+      url "https://github.com/Panchangam18/test-cloudscript-brew/releases/download/v0.0.9/cloud-cli-linux-amd64.tar.gz"
       sha256 "f707d6179fcba3b6b063f3b9f080279056f242d10c6d409e4b9c0e03e620387f"
     end
   
